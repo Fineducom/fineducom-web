f27e9c67fcfe07d70da5c927461df8fdc4a84934
Angelo Almeida <angelo94.aa@gmail.com>
2023-02-14 10:11:51 -0500

    agregando carpeta app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,18 @@
 import '@styles/globals.css'
 import Layout from '@components/Layout'
+import Script from 'next/script'
 export default function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
 			<Component {...pageProps} />
+			<Script
+				type="module"
+				src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+			/>
+			<Script
+				nomodule
+				src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+			/>
 		</Layout>
 	)
 }
